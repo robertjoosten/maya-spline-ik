8014c24aa034033225f6051a7b8db54954e8ebae
Robert Joosten <rwm.joosten@gmail.com>
2017-10-16 22:13:18 +0100

    documentation

--- __init__.py
+++ __init__.py
@@ -37,10 +37,22 @@ The Spline IK module works on a curve and generates an joint chain that
 sticks to it's position on the curve. This means that stretch and squash will
 only occur in the areas that manipulates as opposed to it scaling as a whole.
 
+.. figure:: https://github.com/robertjoosten/rjSplineIK/raw/master/data/stretchSquash.gif
+   :align: center
+   Stretch and Squash demo
+   
 The other benefit of using this module over a regular spline IK is the fact 
 that the twist is divided over the controls that are generated and not just 
 limited to the beginning and end.
-        
+
+.. figure:: https://github.com/robertjoosten/rjSplineIK/raw/master/data/partialTwist.gif
+   :align: center
+   Partial Twist demo
+
+.. figure:: https://github.com/robertjoosten/rjSplineIK/raw/master/data/shift.gif
+   :align: center
+   Shift demo
+      
 The Spline IK class is flexible for creation, before the create function
 is called some other attributes can be changed as well.
 
